7072eff23ed91676d5d5ce3719aa3f9cd9977560
samourai-lzt <78436325+samourai-lzt@users.noreply.github.com>
2021-07-11 19:35:35 +0300

    V0.1.2
    Testing new features with async/await and promises

--- sorter.js
+++ sorter.js
@@ -3,64 +3,87 @@ const fs = require('fs');
 const fs2 = require('fs-extra');
 const colors = require('colors');
 const glob = require('glob');
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-console.clear();
-console.log('');
-console.log(' ▄▀█ █ █ █▀▀ █▀ █▀█ █▀█ ▀█▀ █▀▀ █▀█'.yellow);
-console.log(' █▀█ ▀▄▀ ██▄ ▄█ █▄█ █▀▄  █  ██▄ █▀▄'.yellow);
-console.log(' with luv by samourai lzt <3'.red);
-console.log('')
+function logo() {
+    console.clear();
+    console.log('');
+    console.log(' ▄▀█ █ █ █▀▀ █▀ █▀█ █▀█ ▀█▀ █▀▀ █▀█ testing'.yellow);
+    console.log(' █▀█ ▀▄▀ ██▄ ▄█ █▄█ █▀▄  █  ██▄ █▀▄'.yellow);
+    console.log(' with luv by samourai lzt <3'.red);
+    console.log('')
+}
 
 function text_show() {
+    return new Promise((resolve, reject) => {
     console.log('');
     console.log(' Looking for next requests:'.cyan);
     console.log('');
-}
-
-function zapros_show() {
-    fs.readFile('requests.txt', 'utf8', async function(err, data) {
+    fs.readFile('requests.txt', 'utf8', function(err, data) {
         const zapros = data.toString().split('\n').toString().split('\r,');
-        console.log(zapros)
+        console.log(zapros);
+        console.log('');
+        resolve("Ok");
+        });
     });
 }
 
-function text_show2() {
-    console.log('');
-    console.log(' Reading dirs:'.cyan);
-    console.log('');
-}
-
-function text_show3() {
-    console.log('');
-    console.log(' Sorting logs...'.cyan);
-    console.log('');
-}
-
-async function main() {
-    fs.readFile('requests.txt', 'utf8', async function(err, data) {
-        const zapros = data.toString().split('\n').toString().split('\r,');
-        glob(`${check_folder}/**/**/*asswords.txt`, function(err, files) {
-            for (let i = 0; i < files.length; i++) {
-                let data = fs.readFileSync(`${files[i]}`, 'utf8');
-                zapros.forEach(request => {
-                    if(data.includes(request)) {
-                        let tempstring = files[i].split(`${check_folder}/`)[1];
-                        let result = tempstring.substring(0, tempstring.search('/'));
-                        fs2.copy(`${check_folder}/${result}/`, `results/${request}/${result}/`, function (err) {
-                            if (err) return console.log(err)
-                            console.log(` [${request}]`, `${result}`.green, `is saved`.yellow)
-                        });
-                    }
+function main() {
+    return new Promise((resolve, reject) => {
+        rl.question(`${` Which format of saving results?`.cyan}${`\n\n 1. Save full log per request`.yellow}${`\n 2. Save only passwords per request`.yellow}${`\n 3. Collect each request to one file`.yellow}${`\n\n Answer: `.cyan}`, answer => {
+        rl.close()
+        switch(parseInt(answer)) {
+            case 1:
+                console.clear();
+                logo();
+                console.log(` ${`Your choice is: `.cyan}${answer}`)
+                console.log('');
+                fs.readFile('requests.txt', 'utf8', function(err, data) {
+                    const zapros = data.toString().split('\n').toString().split('\r,');
+                    glob(`${check_folder}/**/**/*asswords.txt`, function(err, files) {
+                        for (let i = 0; i < files.length; i++) {
+                            let data = fs.readFileSync(`${files[i]}`, 'utf8');
+                            zapros.forEach(request => {
+                                if(data.includes(request)) {
+                                    let tempstring = files[i].split(`${check_folder}/`)[1];
+                                    let result = tempstring.substring(0, tempstring.search('/'));
+                                    fs2.copy(`${check_folder}/${result}/`, `results/${request}/${result}/`, function (err) {
+                                        if (err) return console.log(err)
+                                        console.log(` [${request}]`, `${result}`.green, `is saved`.yellow)
+                                    });
+                                }
+                            });
+                        }
+                    });  
                 });
-            }
-        });  
+                break;
+            case 2:
+                logo();
+                console.log(` ${`Error`.bgRed}${ ` In developing`.cyan}`);
+                console.log('');
+                break;
+            case 3:
+                logo();
+                console.log(` ${`Error`.bgRed}${ ` In developing`.cyan}`);
+                console.log('');
+                break;
+            default:
+                logo();
+                console.log(` ${`Error`.bgRed}${ ` Invalid number`.cyan}`);
+                console.log('');
+                break;
+        }
     });
-    
+    });
 }
 
-setTimeout(text_show, 1000);
-setTimeout(zapros_show, 1200);
-setTimeout(text_show2, 2100);
-setTimeout(text_show3, 2600);
-setTimeout(main, 2900);
-return 1
+logo();
+text_show().then(async(result) => {
+    if (result == "Ok") {
+        main();
+    }
+})
